3b73332940cb1152f1cd73f06adbd9ed81af4744
hahahaha3 <eksql2689@gmail.com>
2022-02-21 10:57:39 +0900

    다시수정..

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -3,7 +3,7 @@
 export default function Visual() {
     const path = process.env.PUBLIC_URL;
     return (
-        <figure id="visual_figure" className="myScroll">
+        <figure id="visual_figure">
             <video src={`${path}/img/visual6.mp4`} autoPlay loop muted>
             </video>
             <div className="txtBox">
