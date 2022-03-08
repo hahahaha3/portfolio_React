ae6e089796fa59f99c5892f8a1cdda508b0684ee
hahahaha3 <eksql2689@gmail.com>
2022-03-08 23:46:30 +0900

    info 수정

--- src/components/main/Info.js
+++ src/components/main/Info.js
@@ -1,17 +1,20 @@
 export default function Info({scrolled, posStart, posEnd}) {
-    const base = -1000;
+    const base = -480;
     const start = posStart + base;
     const position = scrolled - start;
+    console.log(start);
+    console.log(position);
+    console.log(scrolled);
 
     return (
         <section id="info" className="myScroll">
-            <h1 style={
+            <div style={
                 scrolled >= start
                 ?
                 {transform: `translateX(${position}px)`}
                 :
-                null
-            }>Mission</h1>
+                null} className="box"></div>
+            <h1>Mission</h1>
             <strong>“</strong>
             <h5>In the middle of the Pacific Ocean, lies a tropical island,</h5>
             <h5>land of beauty and adventure, home of the Chamorro people, land of kindess and acceptation</h5>
